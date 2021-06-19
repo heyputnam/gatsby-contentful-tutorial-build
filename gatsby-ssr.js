9358099a40d42c5ff91792d67632cbd8707e7819
Hailey <hailey@MacBook-Pro.local>
2021-06-19 12:31:53 -0400

    ugh

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Layout from "./src/globalStyle"
 
-wrapPageElement = ({ element, props }) => {
+exports.wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
   return <Layout {...props}>{element}</Layout>
 }
 
-
-export default wrapPageElement;

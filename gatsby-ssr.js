5ec93fa4aaf3a2d237b2eccb25ac993ae06fd700
Hailey <hailey@MacBook-Pro.local>
2021-06-19 13:20:46 -0400

    test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,10 @@
-const React = require("react")
-const Layout = require('./src/globalStyle').default
+import React from 'react'
+import Layout from './src/globalStyle'
 
-exports.wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  return <Layout {...props}>{element}</Layout>
-}
+
+export const wrapRootElement = ({ element }) => (
+  <>
+   
+    <Layout>{element}</Layout>
+  </>
+)

48d6454ffa9a0335d4ce55e7056f48f51f3f7949
Hailey <hailey@MacBook-Pro.local>
2021-06-25 21:16:21 -0400

    styled dynamic list and corrected links

--- src/pages/gatsby.js
+++ src/pages/gatsby.js
@@ -1,72 +1,68 @@
-import * as React from "react"
-import styled from "styled-components"
-import { useStaticQuery } from 'gatsby'
-import Layout from '../globalStyle'
+// a page to test a new components/ options
+import React from "react";
+import Layout from '../globalStyle';
+import { graphql, useStaticQuery } from 'gatsby'
+import styled from 'styled-components';
 import gatsby from '../images/gatsby.png'
-import { useState } from 'react'
-import { AnchorLink } from "gatsby-plugin-anchor-links";
+
+
 const TutorialList = styled.div`
 display: flex;
-/* justify-content: center; */
-/* align-items: center; */
+justify-content: center; 
+
 margin-left: 5rem;
 flex-direction: column;
-margin-top: 2rem;
+
+
 mark{
     background: #b491c8;
 }
+
+
+
+.tags{
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+  width: 35rem;
+  height: 7rem;
+
+
+}
+
 `
+const Description = styled.div`
+display: flex;
+width: 75%;
+margin-top: 1rem;
 
-
-
-const ListItem = styled.ul`
+`
+const ListItem = styled.div`
 justify-content: center;
 /* align-items: center; */
 list-style-type: none;
+
 margin-top: 2rem;
-
-
-.description {
-width: 30rem;
-margin-top: 1rem;
-}
-
-
 a{
 
     text-decoration: none;
     color: black;
     font-size: 1.7rem;
 }
-
-
-
 a:hover{
     font-size: 2.3rem;
     margin-left: -2rem;
+    background: orange;
     cursor: url("https://i.ibb.co/fStr5rr/index-finger.png"),  auto;
    
 }
 
 
-.container{
-    display: flex;
-    flex-direction: column;
-    margin-top: 2rem;
-    
-}
 
-.tags{
-    display: flex;
 
-  flex-direction: row;
-  
-}
 
 `
-
 const Tag = styled.div`
-
+display: flex;
 height: 2rem;
 width: 7rem;
 background: #FE9BF4;
@@ -75,12 +71,11 @@ margin-right: 0.5rem;
 padding-top: 0.6rem;
 box-shadow: 6px 6px 6px ;
 font-size: 0.7rem;
-display: flex;
-
 padding-left: 0.7rem;
 
 &:hover{
-    background: #F955E8;
+    /* background: #F955E8; */
+    background: #F9FE62;
     /* margin-left: 1rem; */
     margin-right: 1rem;
 
@@ -90,14 +85,7 @@ padding-left: 0.7rem;
     
 }
 
-span{
-    display: absolute;
-    right: 0;
-    margin-top: -0.8rem;
-    margin-left: 1.3rem;
-    /* padding-right: 0.2rem; */
-    font-size: 1.5rem;
-}
+
 
 `
 const Title = styled.div`
@@ -105,8 +93,7 @@ margin-top: 4.5rem;
 font-size: 2rem;
 text-align: center;
 
-img{
-    position: fixed;
+ img{
     height: 5rem;
     width: 5.5rem;
     margin-left: 2rem;
@@ -120,77 +107,86 @@ img:hover{
 
 mark{
     background: #b491c8;
-}
+} 
 
 p{
-    font-size: 2rem;
+    font-size: 1.5rem;
 
     
 }
 
-.small {
-    margin-top: 0.5rem;
-  font-size: 1.3rem;
-}
 `
+const TagContainer = styled.div`
+display: flex;
+flex-direction: row;
+background: black;
 
+`
 const Container = styled.div`
-/* margin-top: 3rem; */
-
-height: 41vw;
+/* height: 75vw; */
 `
 
-
-
-const TutorialsPage = ({children}) => {
-    
- 
-
-    
-    return (
-    
-<>
-<Layout>
-
-     <Title>
-     <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+const GatsbyList = ({children}) =>{
+ const result = useStaticQuery(graphql`
+    query {
+     allContentfulLesson(filter: {list: {eq: "gatsby"}}){
+                         nodes {
+                           slug
+                           name
+                           list
+                        description
+                   tags
+               }
+           }    
+        }
+    `)
+const list = result.allContentfulLesson.nodes.map( r => r)
+const listList = list
+return(
+  <>  
+  <Layout/>
+  <Container>
+  <TutorialList>
+   <Title> 
+     <div style={{
+       display: "flex", 
+       flexDirection: "row", 
+       justifyContent: "center"
+       }}>
          <h1>the Great <mark>Gatsby.js</mark></h1>
-           <a href="https://www.gatsbyjs.com/"> <img src={gatsby} height={20} width={20}></img></a>
-            </div>
-          <p className="small">to view a tutorial simply on the link!</p>
-      </Title>
-      <Container>
-      <TutorialList>
+           <a href="https://www.gatsbyjs.com/"> 
+           <img src={gatsby} height={20} width={20}></img>
+           </a>
+        </div>
+          <p className="small">
+              to view a tutorial simply on the link!
+              </p>
+   </Title>
+    {listList.map(list=>{       
+      return(
+        <>
         <ListItem>
-            <div className="container">
-      <AnchorLink to="/tutorials/gatsby-contentful101/#top" >contentful and <mark>gatsby</mark> 101</AnchorLink>
-            <div className="description">
-                <p>learn the basics of contentful with gatsby.js, using staticQueries, contentModels, and learning graphql</p>
-            </div>
-            <div className="tags">
-            <Tag>#graphql</Tag> 
-            <Tag>#contentmodel</Tag> 
-            <Tag>#dynamicdata</Tag> 
-            <Tag>#staticquery</Tag> 
-            </div>
-            </div>
+         <a 
+        href={`/tutorials/${list.slug}/#start`}
+        >{list.name}
+        </a>
         </ListItem>
-        <ListItem>
-            <div className="container">
-            <li><a href="/404"><mark>fake</mark> list item</a></li>
-            <div className="tags">
-            <Tag>#fake</Tag> 
-            <Tag>#fake</Tag> 
-            <Tag>#fake</Tag> 
-            <Tag>#fake</Tag> 
-            </div>
-            </div>
-        </ListItem>
-      </TutorialList>
-      </Container>
-      </Layout>
- </>
-    )
-  }
-  
-  export default TutorialsPage;
+       <Description>{list.description}</Description>
+      <div className="tags">
+        {list.tags.map(l=>{
+       return(
+         <Tag>#{l}</Tag>
+       )
+     })}
+    </div>
+       </> 
+         )
+            })}
+</TutorialList>
+</Container>
+  </>
+)
+}
+
+
+export default GatsbyList;
--- src/pages/test.js
+++ src/pages/test.js
@@ -2,41 +2,16 @@
 import React from "react";
 import Layout from '../globalStyle';
 import { graphql, useStaticQuery } from 'gatsby'
+import styled from 'styled-components';
 
 
 const Test = ({children}) =>{
- const result = useStaticQuery(graphql`
-    query {
-     allContentfulLesson(filter: {list: {eq: "gatsby"}}){
-                             nodes {
-                                slug
-                                 name
-                                 list
-                                }
-                              }    
-               }
-         
-  
-    
-    `)
-const list = result.allContentfulLesson.nodes.map( r => r)
-const listList = list.reverse()
-return(
-  <>  
-  <Layout/>
-  <div style={{margin: "10rem"}}>
-   <h1>Example on how to dynamically render the list data </h1>
-    {listList.map(list=>{
- 
-      return(
-        <div>
-        <li><a href={`/tutorials/${list.slug}`}>{list.name}</a></li>
-        </div>
-      )
-    })}
 
-</div>
-    </>
+return(
+ <>
+  <Layout/>
+  
+  </>
 )
 }
 
